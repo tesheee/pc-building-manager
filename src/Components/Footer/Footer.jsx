208eec24f6de453bb74adf77e63b4058f9305cbe
Tesheee <mrsonohoka@gmail.com>
2022-12-13 10:53:50 +0300

    Version 0.3.3

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { faTelegram, faVk } from "@fortawesome/free-brands-svg-icons"
 import footerStyles from "./Footer.module.css";
 
 
@@ -7,14 +8,14 @@ function Footer(){
     return (
         <footer>
             <div className="container">
-                <div className={footerStyles.footer__content}>
+                <div className={"footer__content"}>
                     <div className="contacts">
                         <h1>Контакты</h1>
                         <p>+79996667536</p>
                         <h1>Cоц. сети</h1>
-                        <div className={footerStyles.social__network__icons}>
-                            <a></a>
-                            <a href="https://vk.com/id473606599"></a>
+                        <div className={"social__network__icons"}>
+                            <a><FontAwesomeIcon icon={faTelegram} size={"2x"}/></a>
+                            <a href="https://vk.com/id473606599"><FontAwesomeIcon icon={faVk} size={"2x"}/></a>
                         </div>
                     </div>
                     <div className="copyright"><p>Copyright © GlekTeam.com, 2022</p></div>
