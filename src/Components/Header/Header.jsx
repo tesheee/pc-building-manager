4dc727ec344e6ac15f6c92ab7c4dae9bca291250
Tesheee <70746364+TxTFexe@users.noreply.github.com>
2022-12-16 01:02:39 +0300

    Update Header.jsx

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,31 +4,30 @@ import {faCartShopping, faHeart, faUser} from "@fortawesome/free-solid-svg-icons
 import NavItemCount from "../NavItemCount";
 import { Link } from "react-router-dom";
 import ModalLogin from "../ModalLogin";
-import headerStyles from "./Header.module.css";
 import Search from "./Search";
 
 function Header(){
     const [modalActive, setModalActive] = useState(false);
 
     return (
-        <header className={headerStyles.header}>
+        <header className="header">
             <div className="container">
-                <div className={headerStyles.header__nav}>
-                    <Link to="/" className={headerStyles.logo}>Хагенти</Link>
+                <div className="header__nav">
+                    <Link to="/" className={"logo"}>Хагенти</Link>
                     <Search/>
-                    <nav className={headerStyles.nav}>
-                        <ul className={headerStyles.nav__list}>
+                    <nav className="nav">
+                        <ul className="nav__list">
                             <li className="nav-item">
                                 <button onClick={() => setModalActive(!modalActive)}><FontAwesomeIcon icon={faUser}/></button>
                                 {modalActive && <ModalLogin state={() => setModalActive(!modalActive)}/>}
                             </li>
                             <li className="nav-item">
-                                <FontAwesomeIcon className="nav-item-icon" icon={faHeart}/>
+                                <Link to={"/"}><FontAwesomeIcon className="nav-item-icon" icon={faHeart}></FontAwesomeIcon></Link>
                                 <Link to={"/"} className="nav-link">Избранное</Link>
                             </li>
                             <li className="nav-item">
                                 <NavItemCount/>
-                                <FontAwesomeIcon className="nav-item-icon" icon={faCartShopping}/>
+                                <Link to={"/Cart"}><FontAwesomeIcon className="nav-item-icon" icon={faCartShopping}/></Link>
                                 <Link to={"/Cart"} className="nav-link">Корзина</Link>
                             </li>
                         </ul>
