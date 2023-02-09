c4482f6aa24c43da391788b9d10a5b83f6f4f8f2
Tesheee <70746364+TxTFexe@users.noreply.github.com>
2023-02-09 22:56:12 +0300

    Update CategoryGPU.js

--- src/Pages/CategoryGPU.js
+++ src/Pages/CategoryGPU.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-//import axios from "axios";
+import axios from "axios";
 import { Card, Pagination, Sort } from "../Components";
 import { SearchContext } from "../App";
 import { useDispatch, useSelector } from "react-redux";
@@ -14,14 +14,14 @@ function CategoryGPU() {
 
     React.useEffect(() => {
         
-        /*axios
+        axios
         .get(
             `https://6395a92790ac47c680703bcd.mockapi.io/GPU?page=${currentPage}&limit=4`
         )
         .then((res) => {
             setItems(res.data);
             setIsLoading(false);
-        });*/
+        });
 
     }, [currentPage])
 
